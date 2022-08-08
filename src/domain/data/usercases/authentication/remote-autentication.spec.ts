fa038fb8f54ab6c22b6420cde0d0f0bd90aeea4d
Waldener Monteiro <waldener.dev@gmail.com>
2022-08-08 20:38:59 -0300

    chore: add faker

--- src/domain/data/usercases/authentication/remote-autentication.spec.ts
+++ src/domain/data/usercases/authentication/remote-autentication.spec.ts
@@ -1,13 +1,14 @@
 
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
+import { faker } from '@faker-js/faker'
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url: string = 'any_url'): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
 
