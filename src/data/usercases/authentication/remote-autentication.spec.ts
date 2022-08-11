119cc92975684798f683505ad6e942882e19fa0a
Waldener Monteiro <waldener.dev@gmail.com>
2022-08-10 21:46:44 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- src/data/usercases/authentication/remote-autentication.spec.ts
+++ src/data/usercases/authentication/remote-autentication.spec.ts
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker'
 
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { mockAuthentication } from '@/domain/test/mock-authentication'
+import { mockAccountModel, mockAuthentication } from '@/domain/test/mock-account'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
@@ -82,5 +82,17 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(authenticationParams)
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const httpResult = mockAccountModel()
+
+    httpPostClientSpy.response = { statusCode: HttpStatusCode.ok, body: httpResult }
+
+    const authenticationParams = mockAuthentication()
+
+    const account = await sut.auth(authenticationParams)
+
+    expect(account).toEqual(httpResult)
   })
 })
--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,10 +1,17 @@
 import { faker } from '@faker-js/faker'
 
 import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { AccountModel } from '../models/account-model'
 
 export const mockAuthentication = (): AuthenticationParams => {
   return {
     email: faker.internet.email(),
     password: faker.internet.email()
+  }
+}
+
+export const mockAccountModel = (): AccountModel => {
+  return {
+    accessToken: faker.random.alphaNumeric()
   }
 }
