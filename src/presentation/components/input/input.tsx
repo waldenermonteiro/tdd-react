65837d6d2cc4d998fba19147a635fd2e654b3c65
Waldener Monteiro <waldener.dev@gmail.com>
2022-08-20 18:32:35 -0300

    feat: ensure Login present error if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -17,11 +17,11 @@ const Input: React.FC<Props> = (props: Props) => {
   const error = state[`${props.name}Error`]
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo Certo!'
   }
 
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
